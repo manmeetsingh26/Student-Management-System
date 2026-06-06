b9ddcd9dd00755b66d4356f7bd4c91169ec87163
Manmeet Singh <manmeet110907@gmail.com>
2026-06-06 09:50:56 +0530

    Implemented delete student functionality

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ int main() {
         cout << "1. Add Student\n";
         cout << "2. View Students\n";
         cout << "3. Search Student\n";
-        cout << "4. Exit\n";
+        cout << "4. Delete Student\n";
+        cout << "5. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -22,7 +23,6 @@ int main() {
 
             case 1:
                 if (studentCount < 100) {
-
                     cout << "Enter Student Name: ";
                     cin >> studentNames[studentCount];
 
@@ -32,7 +32,6 @@ int main() {
                     studentCount++;
 
                     cout << "Student added successfully.\n";
-
                 } else {
                     cout << "Student limit reached.\n";
                 }
@@ -40,15 +39,11 @@ int main() {
 
             case 2:
                 if (studentCount == 0) {
-
                     cout << "No student records found.\n";
-
                 } else {
-
                     cout << "\n===== Student Records =====\n";
 
                     for (int i = 0; i < studentCount; i++) {
-
                         cout << "\nStudent " << i + 1 << endl;
                         cout << "Name: " << studentNames[i] << endl;
                         cout << "Roll Number: " << rollNumbers[i] << endl;
@@ -64,9 +59,7 @@ int main() {
                 cin >> searchRoll;
 
                 for (int i = 0; i < studentCount; i++) {
-
                     if (rollNumbers[i] == searchRoll) {
-
                         cout << "\n===== Student Found =====\n";
                         cout << "Name: " << studentNames[i] << endl;
                         cout << "Roll Number: " << rollNumbers[i] << endl;
@@ -83,7 +76,38 @@ int main() {
                 break;
             }
 
-            case 4:
+            case 4: {
+                int deleteRoll;
+                bool found = false;
+
+                cout << "Enter Roll Number to Delete: ";
+                cin >> deleteRoll;
+
+                for (int i = 0; i < studentCount; i++) {
+                    if (rollNumbers[i] == deleteRoll) {
+
+                        for (int j = i; j < studentCount - 1; j++) {
+                            studentNames[j] = studentNames[j + 1];
+                            rollNumbers[j] = rollNumbers[j + 1];
+                        }
+
+                        studentCount--;
+
+                        cout << "Student deleted successfully.\n";
+
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    cout << "Student not found.\n";
+                }
+
+                break;
+            }
+
+            case 5:
                 cout << "Thank you for using Student Management System.\n";
                 break;
 
@@ -91,7 +115,7 @@ int main() {
                 cout << "Invalid choice. Please try again.\n";
         }
 
-    } while (choice != 4);
+    } while (choice != 5);
 
     return 0;
 }
